96d584975a5748d6e0784a1dc5cbe35d8b245c55
SimpleGitDocs <SimpleGitDocs@example.com>
2024-08-09 05:40:33 +0000

    Deploy to gh-pages

--- wiki.js
+++ wiki.js
@@ -21,11 +21,15 @@ function RandomShow(HTMLId,len)
 for(var i=0;i<len;i++)
 {document.getElementById(HTMLId+"_"+i).style.display=i==random?"block":"none";}}
 fetch_files()
+let lastSearchText=""
 document.addEventListener("DOMContentLoaded",function(){document.getElementById("search").addEventListener("input",function(){var search=document.getElementById("search").value;var suggestions=GetSuggestions(search,5)
+if(suggestions.length==0&&Math.abs(lastSearchText.length-search.length)<=2)
+return
 var suggestionList=document.getElementById("Suggestions")
 suggestionList.innerHTML=""
 for(var i=0;i<suggestions.length;i++){var suggestion=document.createElement("div")
 const SplitedSuggest=suggestions[i].split("/")
 suggestion.innerText=SplitedSuggest[SplitedSuggest.length-1]
 suggestion.classList.add("Suggestion");var url="/"+suggestions[i]
-suggestion.addEventListener("click",function(){window.location.href=url});suggestionList.appendChild(suggestion)}});document.getElementById("Light-DarkToggleButton").addEventListener("click",function(){document.body.classList.toggle("Dark");});});
+suggestion.addEventListener("click",function(){window.location.href=url});suggestionList.appendChild(suggestion)}
+lastSearchText=search});document.getElementById("Light-DarkToggleButton").addEventListener("click",function(){document.body.classList.toggle("Dark");});});
