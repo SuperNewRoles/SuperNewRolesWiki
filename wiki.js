601cba3aee83222a943c6bb77bed4b06fb83d133
ykundesu <4476tv@gmail.com>
2024-08-09 19:43:51 +0900

    Headdings子要素を追加

--- wiki.js
+++ wiki.js
@@ -114,6 +114,21 @@ document.addEventListener("DOMContentLoaded", function () {
             UpdateStorage("DarkMode", "false");
         }
     });
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            const headerOffset = 75; // 固定ヘッダーの高さに合わせる
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
     UpdateByStorage();
     UpdateHeadding();
 });
