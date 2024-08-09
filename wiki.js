8ff3326fc7b037024c04dda6e8db629b3747cf33
SimpleGitDocs <SimpleGitDocs@example.com>
2024-08-09 07:44:06 +0000

    Deploy to gh-pages

--- wiki.js
+++ wiki.js
@@ -1,36 +1,25 @@
-var files=[]
-async function fetch_files()
-{await fetch('/documents.list').then(res=>res.text()).then(res=>{files=res.split("\n")});}
+var files=[];async function fetch_files()
+{await fetch('/documents.list').then(res=>res.text()).then(res=>{files=res.split("\n");});}
 function KatakanaToHiragana(str)
 {return str.replace(/[\u30a1-\u30f6]/g,function(match)
 {var chr=match.charCodeAt(0)-0x60;return String.fromCharCode(chr);});}
 function GetSuggestions(query,max)
-{console.log(query)
-var suggestions=[]
-for(var i=0;i<files.length;i++)
-{const query_changed=KatakanaToHiragana(query.toLowerCase())
-const fileSplited=files[i].toLowerCase().split("/")
-const file=KatakanaToHiragana(fileSplited[fileSplited.length-1])
-if(file.includes(query_changed))
-{suggestions.push(files[i])}
+{var suggestions=[];for(var i=0;i<files.length;i++)
+{const query_changed=KatakanaToHiragana(query.toLowerCase());const fileSplited=files[i].toLowerCase().split("/");const file=KatakanaToHiragana(fileSplited[fileSplited.length-1]);if(file.includes(query_changed))
+{suggestions.push(files[i]);}
 if(suggestions.length>=max)
-{break}}
-return suggestions}
+{break;}}
+return suggestions;}
 function RandomShow(HTMLId,len)
-{var random=Math.floor(Math.random()*len)
-for(var i=0;i<len;i++)
+{var random=Math.floor(Math.random()*len);for(var i=0;i<len;i++)
 {document.getElementById(HTMLId+"_"+i).style.display=i==random?"block":"none";}}
 function UpdateByCookie()
 {var cookies=document.cookie.split(";");for(var i=0;i<cookies.length;i++){var cookie=cookies[i].split("=");if(cookie[0].trim()=="DarkMode"&&cookie[1].trim()=="true"){document.body.classList.add("Dark");}}}
 function UpdateCookie(key,value)
 {var date=new Date();date.setTime(date.getTime()+1000*60*60*24*365);document.cookie=key+"="+value+"; expires="+date.toUTCString();}
-fetch_files()
-let lastSearchText="";document.addEventListener("DOMContentLoaded",function(){document.getElementById("search").addEventListener("input",function(){var search=document.getElementById("search").value;if(search.length==0)
+fetch_files();let lastSearchText="";document.addEventListener("DOMContentLoaded",function(){document.getElementById("search").addEventListener("input",function(){var search=document.getElementById("search").value;if(search.length==0)
 {document.getElementById("Suggestions").innerHTML=""
 lastSearchText="";return;}
-var suggestions=GetSuggestions(search,5)
-if(suggestions.length==0&&Math.abs(lastSearchText.length-search.length)<2&&search.length>1)
-return
-var suggestionList=document.getElementById("Suggestions")
-suggestionList.innerHTML="";for(var i=0;i<suggestions.length;i++){var suggestion=document.createElement("a");const SplitedSuggest=suggestions[i].split("/");suggestion.innerText=SplitedSuggest[SplitedSuggest.length-1];suggestion.classList.add("Suggest");var url="/"+suggestions[i];suggestion.setAttribute("href",url);suggestionList.appendChild(suggestion);}
+var suggestions=GetSuggestions(search,5);if(suggestions.length==0&&Math.abs(lastSearchText.length-search.length)<2&&search.length>1)
+return;var suggestionList=document.getElementById("Suggestions");suggestionList.innerHTML="";for(var i=0;i<suggestions.length;i++){var suggestion=document.createElement("a");const SplitedSuggest=suggestions[i].split("/");suggestion.innerText=SplitedSuggest[SplitedSuggest.length-1];suggestion.classList.add("Suggest");var url="/"+suggestions[i];suggestion.setAttribute("href",url);suggestionList.appendChild(suggestion);}
 lastSearchText=search;});document.getElementById("Light-DarkToggleButton").addEventListener("click",function(){document.body.classList.toggle("Dark");if(document.body.classList.contains("Dark")){UpdateCookie("DarkMode","true");}else{UpdateCookie("DarkMode","false");}});UpdateByCookie();});UpdateByCookie();
