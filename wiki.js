4e394faa88c8e076b65212dea0ae3402ec9b09ed
SimpleGitDocs <SimpleGitDocs@example.com>
2024-08-09 08:28:47 +0000

    Deploy to gh-pages

--- wiki.js
+++ wiki.js
@@ -20,11 +20,11 @@ document.body.classList.remove("Dark");}
 function UpdateStorage(key,value)
 {localStorage.setItem(key,value);}
 function UpdateHeadding()
-{var headdings=document.getElementById("Headdings");for(var i=0;i<headding_one.length;i++)
-{var headding=document.createElement("a");headding.innerText=headding_one[i];headding.classList.add("Button");headding.setAttribute("href","#"+headding_one[i]);headdings.appendChild(headding);}}
+{var headdings=document.getElementById("ContentsTableObject");for(var i=0;i<headding_one.length;i++)
+{var headding=document.createElement("a");headding.innerText=headding_one[i];headding.classList.add("Button");headding.setAttribute("href","#"+headding_one[i]);headdings.appendChild(headding);headdings.appendChild(document.createElement("br"));}}
 fetch_files();let lastSearchText="";document.addEventListener("DOMContentLoaded",function(){document.getElementById("search").addEventListener("input",function(){var search=document.getElementById("search").value;if(search.length==0)
 {document.getElementById("Suggestions").innerHTML=""
 lastSearchText="";return;}
 var suggestions=GetSuggestions(search,5);if(suggestions.length==0&&Math.abs(lastSearchText.length-search.length)<2&&search.length>1)
 return;var suggestionList=document.getElementById("Suggestions");suggestionList.innerHTML="";for(var i=0;i<suggestions.length;i++){var suggestion=document.createElement("a");const SplitedSuggest=suggestions[i].split("/");suggestion.innerText=SplitedSuggest[SplitedSuggest.length-1];suggestion.classList.add("Suggest");var url="/"+suggestions[i];suggestion.setAttribute("href",url);suggestionList.appendChild(suggestion);}
-lastSearchText=search;});document.getElementById("Light-DarkToggleButton").addEventListener("click",function(){document.body.classList.toggle("Dark");if(document.body.classList.contains("Dark")){UpdateStorage("DarkMode","true");}else{UpdateStorage("DarkMode","false");}});UpdateByStorage();console.log(headding_one);UpdateHeadding();});console.log(headding_one);UpdateHeadding();
+lastSearchText=search;});document.getElementById("Light-DarkToggleButton").addEventListener("click",function(){document.body.classList.toggle("Dark");if(document.body.classList.contains("Dark")){UpdateStorage("DarkMode","true");}else{UpdateStorage("DarkMode","false");}});UpdateByStorage();UpdateHeadding();});
