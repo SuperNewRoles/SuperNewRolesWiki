b79e1014decd7a1a02b120daa2aff2bda78bad24
SimpleGitDocs <SimpleGitDocs@example.com>
2024-08-09 10:26:16 +0000

    Deploy to gh-pages

--- wiki.js
+++ wiki.js
@@ -22,8 +22,8 @@ function UpdateStorage(key,value)
 function UpdateHeadding()
 {var headdings=document.getElementById("Headdings");for(var key in headding_dict)
 {if(key!="")
-Generate_Headding(key);for(var i=0;i<headding_dict[key].length;i++){Generate_Headding(headding_dict[key][i]);}}}
-function Generate_Headding(key)
+Generate_Headding(key,headdings);for(var i=0;i<headding_dict[key].length;i++){Generate_Headding(headding_dict[key][i],headdings);}}}
+function Generate_Headding(key,headdings)
 {var headding=document.createElement("a");headding.innerText=key;headding.classList.add("Button");headding.setAttribute("href","#"+key);headdings.appendChild(headding);}
 fetch_files();let lastSearchText="";document.addEventListener("DOMContentLoaded",function(){document.getElementById("search").addEventListener("input",function(){var search=document.getElementById("search").value;if(search.length==0)
 {document.getElementById("Suggestions").innerHTML=""
